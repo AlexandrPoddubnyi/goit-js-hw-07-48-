19e0c058f5a04cde76fb382c8d56e1294d1a5d47
AlexandrPoddubnyi <San4oss91@gmail.com>
2022-07-03 11:33:20 +0300

    HW part2 done JS+HTML

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,3 +3,21 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const galleryList = document.querySelector('.gallery');
+const galleryItem = createGalleryItem(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryItem);
+
+function createGalleryItem(image) {
+  return image.map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+              </a>`
+  }).join('')
+}
+
+var gallery = new SimpleLightbox('.gallery a', {
+  captionClass: 'galley__item',
+  captionsData: 'alt',
+  captionDelay: 250
+})
